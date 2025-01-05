853bb4b7e88179398f11dbc93a3c16efc1bd95b1
WidthDoctor <elskel1812@gmail.com>
2025-01-05 11:13:28 +0100

    feat: add about section

--- src/Pages/General/GeneralPage.tsx
+++ src/Pages/General/GeneralPage.tsx
@@ -1,14 +1,16 @@
-import { useState } from "react";
+
 import "./GeneralPage.css";
 
 import MainMenu from "../MainMenu/MainMenu.tsx";
 import SectionTitle from "../General/SectionTitle.tsx";
+import About from "../General/About.tsx";
 
 function General() {
   return (
     <>
       <MainMenu />
       <SectionTitle />
+      <About />
     </>
   );
 }
